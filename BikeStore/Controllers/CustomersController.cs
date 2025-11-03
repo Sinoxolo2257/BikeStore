1866cabeb056c89717a1a0078e6d4b42f0e886ca
u22579428 <u22579428@INFGY-34>
2025-11-03 16:04:29 +0200

    controller

--- BikeStore/Controllers/CustomersController.cs
+++ BikeStore/Controllers/CustomersController.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using BikeStore.Models;
 
 namespace BikeStore.Controllers
 {
     public class CustomersController : Controller
     {
-        private BikeStoreContext db = new BikeStoreContext();
+        private BikeStoresEntities db = new BikeStoresEntities();
         // GET: Customers
         public async Task<ActionResult> Index()
         {
-            var customers = await db.Customers.ToListAsync();
+            var customers = await db.customers.ToListAsync();
             return View(customers);
         }
 
@@ -23,7 +28,7 @@ namespace BikeStore.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Customer customer = await db.Customers.FindAsync(id);
+            customer customer = await db.customers.FindAsync(id);
             if (customer == null)
             {
                 return HttpNotFound();
@@ -38,19 +43,79 @@ namespace BikeStore.Controllers
         // POST: Customers/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "customer_Id,first_Name,last_Name,email,phone")] Customer customer)
+        public async Task<ActionResult> Create([Bind(Include = "customer_Id,first_Name,last_Name,email,phone")] customer customer)
         {
             if (ModelState.IsValid)
             {
-                db.Customers.Add(customer);
+                db.customers.Add(customer);
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
             return PartialView("_Create", customer);
         }
-        public ActionResult Index()
+
+        // GET: Customers/Edit
+        public async Task<ActionResult> Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = await db.customers.FindAsync(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_Edit", customer);
         }
+
+        // POST: Customers/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "customer_Id,first_Name,last_Name,email,phone")] customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return PartialView("_Edit", customer);
+        }
+
+        // GET: Customers/Delete
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = await db.customers.FindAsync(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_Delete", customer);
+        }
+
+        // POST: Customers/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            customer customer = await db.customers.FindAsync(id);
+            db.customers.Remove(customer);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
     }
 }
--- BikeStore/Controllers/StaffController.cs
+++ BikeStore/Controllers/StaffController.cs
@@ -1,17 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using BikeStore.Models;
 
 namespace BikeStore.Controllers
 {
     public class StaffController : Controller
     {
+        private BikeStoresEntities db = new BikeStoresEntities();
         // GET: Staff
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var staff = await db.staffs.Include(s => s.store).ToListAsync();
+            return View(staff);
+        }
+
+        //GET: Staff/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            staff staff = await db.staffs.FindAsync(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+        // GET: Staff/Create
+        public ActionResult Create()
+        {
+            ViewBag.store_id = new SelectList(db.stores, "store_id", "store_name");
+            return PartialView("_Create"); //modal creation uses parial view
+        }
+        // POST: Staff/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "staff_id,first_name,last_name,email,phone,active,store_id,manager_id")] staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.staffs.Add(staff);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.store_id = new SelectList(db.stores, "store_id", "store_name", staff.store_id);
+            return PartialView("_Create", staff);
+        }
+
+        // GET: Staff/Edit
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            staff staff = await db.staffs.FindAsync(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.store_id = new SelectList(db.stores, "store_id", "store_name", staff.store_id);
+            return PartialView("_Edit", staff);
+        }
+
+        // POST: Staff/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "staff_id,first_name,last_name,email,phone,active,store_id,manager_id")] staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(staff).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.store_id = new SelectList(db.stores, "store_id", "store_name", staff.store_id);
+            return PartialView("_Edit", staff);
+        }
+
+        // POST: Staff/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            staff staff = await db.staffs.FindAsync(id);
+            db.staffs.Remove(staff);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
