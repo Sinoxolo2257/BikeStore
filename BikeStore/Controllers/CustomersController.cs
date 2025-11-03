529e42c70b7813aa4f3635731c87b9fe7c45f521
u22579428 <u22579428@INFGY-34>
2025-11-03 14:20:48 +0200

    controller

---
+++ BikeStore/Controllers/CustomersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikeStore.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private BikeStoreContext db = new BikeStoreContext();
+        // GET: Customers
+        public async Task<ActionResult> Index()
+        {
+            var customers = await db.Customers.ToListAsync();
+            return View(customers);
+        }
+
+        //GET: Customers/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = await db.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+        // GET: Customers/Create
+        public async Task<ActionResult> Create()
+        {
+            return PartialView("_Create");
+        }
+        // POST: Customers/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "customer_Id,first_Name,last_Name,email,phone")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return PartialView("_Create", customer);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BikeStore/Controllers/HomeController.cs
+++ BikeStore/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Data;
+using System.Threading.Tasks; 
+using BikeStore.Models; 
 
 namespace BikeStore.Controllers
 {
