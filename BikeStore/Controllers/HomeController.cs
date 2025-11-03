95b6d0e16641dcb6a68543ccc1a12b6585f563d8
u22579428 <u22579428@INFGY-34>
2025-11-03 16:26:14 +0200

    views

--- BikeStore/Controllers/HomeController.cs
+++ BikeStore/Controllers/HomeController.cs
@@ -12,23 +12,53 @@ namespace BikeStore.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private BikeStoresEntities db = new BikeStoresEntities();
+        // GET: Home/Index
+        public async Task<ActionResult> Index(string brandFilter, string categoryFilter)
+        {
+            var vm = new HomeVM();
+
+            vm.Staff = await db.staffs.ToListAsync();
+            vm.Customers = await db.customers.ToListAsync();
+
+            var productsQuery = db.products.Include(p => p.brand).Include(p => p.category).AsQueryable();
+
+            if (!string.IsNullOrEmpty(brandFilter))
+            {
+                productsQuery = productsQuery.Where(p => p.brand.brand_name == brandFilter);
+                vm.SelectedBrand = brandFilter;
+            }
+
+            if (!string.IsNullOrEmpty(categoryFilter))
+            {
+                productsQuery = productsQuery.Where(p => p.category.category_name == categoryFilter);
+                vm.SelectedCategory = categoryFilter;
+            }
+
+            vm.Products = await productsQuery.ToListAsync();
+            vm.Brands = await db.brands.OrderBy(b => b.brand_name).ToListAsync();
+            vm.Categories = await db.categories.OrderBy(c => c.category_name).ToListAsync();
+
+            return View(vm);
+        }
+
+        // GET: /Home/Manage
+        public ActionResult Manage()
+        {
+            // This view will include partials/links to Staffs/Customers/Products maintain pages
+            return View();
+        }
+
+        // GET: /Home/Reports
+        public ActionResult Reports()
         {
             return View();
         }
 
-        public ActionResult About()
+        protected override void Dispose(bool disposing)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            if (disposing) db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
---
+++ BikeStore/Models/HomeVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikeStore.Models
+{
+    public class HomeVM
+    {
+        public List<staff> Staff { get; set; }
+        public List<customer> Customers { get; set; }
+        public List<product> Products { get; set; }
+        public List<brand> Brands { get; set; }
+        public List<category> Categories { get; set; }
+
+        //for filters
+        public string SelectedBrand { get; set; }
+        public string SelectedCategory { get; set; }
+    }
+}
